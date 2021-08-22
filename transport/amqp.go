7607cfd7533f4d88680ce7483464448b4be9af52
gospodinzerkalo <darhan020818@gmail.com>
2021-08-22 21:10:15 +0600

    add amqp listener

--- transport/amqp.go
+++ transport/amqp.go
@@ -6,7 +6,6 @@ import (
 	"github.com/gospodinzerkalo/crime_city_api/endpoint"
 	"github.com/nats-io/nats-server/v2/logger"
 	"github.com/streadway/amqp"
-	"log"
 )
 
 type amqpServer struct {
@@ -15,25 +14,57 @@ type amqpServer struct {
 
 type RabbitMQConfig struct {
 	Channel 	*amqp.Channel
-	Queue 		*amqp.Queue
 }
 
-func NewRabbitMqServer(endpoints endpoint.Endpoints, log *logger.Logger, conf RabbitMQConfig) *amqpServer {
-	return &amqpServer{
-		sendCrime: amqpTr.NewPublisher(
-			conf.Channel,
-			conf.Queue,
-			encodeSendCrimeAmqpResponse,
-			decodeSendCrimeAmqpRequest,
-			),
-	}
-}
-
-func failOnError(err error, msg string) {
+func NewRabbitMqServer(endpoints endpoint.Endpoints, log *logger.Logger, conf RabbitMQConfig) error {
+	qSendCrime, err := conf.Channel.QueueDeclare(
+		"send_crime", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
 	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
+		return err
 	}
+
+	qSendCrimeMsgs, err := conf.Channel.Consume(
+		qSendCrime.Name,
+		"",    // consumer
+		false, // autoAck
+		false, // exclusive
+		false, // noLocal
+		false, // noWait
+		nil,   // args
+	)
+
+	qSendCrimeAMQPHandler := amqpTr.NewSubscriber(
+		endpoints.GetCrime,
+		decodeSendCrimeAmqpRequest,
+		encodeSendCrimeAmqpResponse,
+	)
+
+	qSendCrimeListener := qSendCrimeAMQPHandler.ServeDelivery(conf.Channel)
+
+	forever := make(chan bool)
+
+	go func() {
+		for true {
+			select {
+			case uppercaseDeliv := <-qSendCrimeMsgs:
+				log.Noticef("received \"send_crime\" request")
+				qSendCrimeListener(&uppercaseDeliv)
+				uppercaseDeliv.Ack(false)
+			}
+		}
+	}()
+
+	log.Noticef("listening")
+	<-forever
+	return nil
 }
+
 
 func decodeSendCrimeAmqpRequest(context.Context, *amqp.Delivery) (request interface{}, err error) {
 	return nil, nil
